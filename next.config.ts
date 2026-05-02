942ee04b3795f5d262a3e32a4ddc28a4dacd9c82
dev1.it <dev1.it@lidahbuayagroup.com>
2026-05-02 14:58:16 +0700

    feat(refactore) html css murni

--- next.config.ts
+++
@@ -1,7 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  transpilePackages: ["@tsparticles/react", "@tsparticles/slim", "@tsparticles/engine"],
-};
-
-export default nextConfig;
